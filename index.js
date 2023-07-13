3fb8bde97d58838b950b2c64c75cf216625b4794
Chandan2812 <chandanpms@gmail.com>
2023-07-13 15:10:48 +0530

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {connection}=require("./config/db")
 const {userRouter}=require("./route/user.route")
 const cors=require("cors")
 const {empRouter}=require("./route/emp.route")
-const {authenticate}=require("./middleware/auth")
+
 
 
 const app=express()
@@ -12,7 +12,7 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 app.use("/users",userRouter)
-app.use(authenticate)
+
 app.use("/employee",empRouter)
 
 
